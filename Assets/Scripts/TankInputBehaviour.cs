3f1b68fa16d7659fd3b882e73bfd12012a5f1b0a
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2017-11-14 01:01:43 +0700

    Rotate sprite when take an input

--- Assets/Scripts/TankInputBehaviour.cs
+++ Assets/Scripts/TankInputBehaviour.cs
@@ -6,28 +6,39 @@ public class TankInputBehaviour : MonoBehaviour
     // Speed tank movement
     public float Speed = 5;
 
+    Vector3 _direction = Vector3.zero;
+    float _angle = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        var direction = Vector3.zero;
+        var transformDirection = transform.rotation;
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            direction = Vector3.down;
+            _direction = Vector3.down;
+            _angle = 180;
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
-            direction = Vector3.up;
+            _direction = Vector3.up;
+            _angle = 0f;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            direction = Vector3.left;
+            _direction = Vector3.left;
+            _angle = 90f;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            direction = Vector3.right;
+            _direction = Vector3.right;
+            _angle = -90f;
+        }
+        else {
+            _direction = Vector3.zero;
         }
 
-        transform.Translate(direction * Time.deltaTime * Speed);
+        transform.rotation = Quaternion.Euler(0, 0, _angle);
+        transform.position += _direction * Time.deltaTime * Speed;
     }
 }
