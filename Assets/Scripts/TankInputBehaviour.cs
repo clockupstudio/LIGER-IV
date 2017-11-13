c50b0d4f81cd94c167776b5aa404331b9dcd14ce
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2017-11-13 23:49:00 +0700

    Initial project with Tank movement

---
+++ Assets/Scripts/TankInputBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TankInputBehaviour : MonoBehaviour
+{
+
+    // Speed tank movement
+    public float Speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = Vector3.down;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = Vector3.up;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector3.right;
+        }
+
+        transform.Translate(direction * Time.deltaTime * Speed);
+    }
+}
